32d9e58ce6577f0583afc75aabf5862c2f016820
Mallory Woods <malvwoods@gmail.com>
2019-02-10 23:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string.toLowerCase) {
+function sayHiToGrandma(string) {
+ if (string.toLowerCase) {
   return("I can\'t hear you!") }
-  
-  
+}
